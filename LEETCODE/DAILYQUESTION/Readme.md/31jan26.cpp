066f39fe76702ad29695656a695377ec6e5c9fdd
dineshbadal <dineshbadal19092005@gmail.com>
2026-01-31 18:06:22 +0530

    Add solution for next greatest letter problem
    Implement solution for finding the smallest letter greater than target.

---
+++ LEETCODE/DAILYQUESTION/Readme.md/31jan26.cpp
@@ -0,0 +1,15 @@
+//  https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n= letters.size();
+        char ans = letters[0];
+        for( int i=0 ; i<n ;i++)
+        {
+        if(letters[i]>target){
+            ans = letters[i];
+            break;
+        }
+}  
+return ans;  }
+};
